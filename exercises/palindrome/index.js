84ba6c7d3db6d853adcedbb15c2f82a08d82dd03
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-05-07 21:23:08 -0700

    working solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return (str.split('').reverse().join('') === str) ? true : false;
+}
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     return char === str[str.length - i - 1];
+//   })
+// }
