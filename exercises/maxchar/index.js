d543fd10959fe1fe54d9752807ccc58235ca9f5d
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-05-07 22:07:23 -0700

    working solution for maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  var max = 0;
+  var maxChar = '';
+
+  // Create an object and increment counter if the lettery key already exists; if not, create it.
+  for (let char of str) {
+    !chars[char] ? chars[char] = 1 : chars[char]++
+  }
+
+  // Loop through object and find the max value as well as the character that occurs the most
+  for (let char in chars) {
+    if (chars[char] > max) {
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
