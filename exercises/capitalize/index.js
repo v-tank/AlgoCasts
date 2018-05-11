f2fb97fccb73f59185d1a9745ad972a7c6b5897c
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-05-10 22:00:44 -0700

    working solution to capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  var result = str[0].toUpperCase();
+  
+  for (var i = 1; i < str.length; i++) {
+    if(str[i-1] === ' '){
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   var splitArray = str.split(' ');
+
+//   for (var i = 0; i < splitArray.length; i++) {
+//     splitArray[i] = splitArray[i].charAt(0).toUpperCase() + splitArray[i].slice(1);
+//   }
+
+//   return splitArray.join(' ');
+// }
