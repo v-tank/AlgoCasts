b7d804cd97ca706f628a542238292317ebd4880d
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-05-10 21:07:39 -0700

    working anagrams solutions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {  
+  return cleanupString(stringA) === cleanupString(stringB);
+}
+
+function cleanupString(str){
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
 
 module.exports = anagrams;
+
+
+// function anagrams(stringA, stringB) {
+
+//   var objectifiedA = objectifyString(stringA);
+//   var objectifiedB = objectifyString(stringB);
+
+//   if (Object.keys(objectifiedA).length !== Object.keys(objectifiedB).length) {
+//     return false;
+//   } else {
+//     for (let char in objectifiedA) {
+//       if (objectifiedA[char] !== objectifiedB[char]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// function objectifyString(string) {
+//   const chars = {};
+
+//   for (let char of string.replace(/[^\w]/g, "").toLowerCase()) {
+//     !chars[char] ? (chars[char] = 1) : chars[char]++;
+//   }
+//   return chars;
+// }
