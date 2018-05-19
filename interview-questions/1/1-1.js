f52e35610100c53b8456639f19e1e4ee9b05f03b
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-05-19 14:49:37 -0700

    working solutiong to hasUniqueChars

---
+++ interview-questions/1/1-1.js
@@ -0,0 +1,17 @@
+function hasUniqueChars(str) {
+  var bools = [];
+  for (let char of str) {
+    if (bools[char]) {
+      console.log(false);
+      console.log(bools);
+      return false;
+    } 
+    bools[char] = true;
+  }
+  console.log(true);
+  console.log(bools);
+  return true;
+}
+
+hasUniqueChars("palindrome");
+hasUniqueChars("charmander");
