7a361557ef432a8161318f00e448fd98761cfe5e
Vaibhav Tank <vaibhav@MBP.local>
2018-06-06 10:58:12 -0700

    working vowels problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  var counter = 0;
+
+  for (let char of str.toLowerCase()) {
+    if ("aeiou".includes(char)) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
 
 module.exports = vowels;
