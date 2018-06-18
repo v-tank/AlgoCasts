ad12db3fb5fddf0df9f518a1c91b921424e2e18a
Vaibhav Tank <vaibhav@MBP.local>
2018-06-17 21:04:21 -0700

    working solution to tower

--- CodeWars/BuildTower/index.js
+++ CodeWars/BuildTower/index.js
@@ -1,5 +1,20 @@
 function towerBuilder(nFloors) {
   // build here
-  
-}
+  let mid = Math.floor((2 * nFloors - 1) / 2);
+  let array = [];
+  for (let i = 0; i < nFloors; i++) {
+    var string = '';
 
+    for (let j = 0; j < (2 * nFloors - 1); j++) {
+      if (mid - i <= j && mid + i >= j) {
+        string += '*'
+      } else {
+        string += ' '
+      }
+    }
+
+    array.push(string);
+  }
+
+  return array
+}
