b2b3c4c15f6f0465d855723dcd860e036a393051
Vaibhav Tank <vaibhav@MBP.local>
2018-08-11 11:56:42 -0700

    working bubbleSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,8 +1,23 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
+function swap(arr, a, b) {
+  let temp =  arr[b];
+  arr[b] = arr[a];
+  arr[a] = temp;
+
+  return arr;
+}
 
 function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j+1]) {
+        swap(arr, j, j+1);
+      }
+    }
+  }
 
+  return arr;
 }
 
 function selectionSort(arr) {
