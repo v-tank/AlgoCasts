eaf2b476463afc8ebe9b981e620ae7afee14dbaa
Vaibhav Tank <vaibhav@MBP.local>
2018-08-11 13:47:20 -0700

    working selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -8,6 +8,7 @@ function swap(arr, a, b) {
   return arr;
 }
 
+// Time Complexity: O(n^2); Space Complexity: O(1)
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < (arr.length - i - 1); j++) {
@@ -20,8 +21,23 @@ function bubbleSort(arr) {
   return arr;
 }
 
+// Time Complexity: O(n^2); Space Complexity: O(1)
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
 
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+
+    if (i !== min) {
+      swap(arr, i , min);
+    }
+  }
+
+  return arr;
 }
 
 function mergeSort(arr) {
@@ -32,4 +48,4 @@ function merge(left, right) {
 
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
