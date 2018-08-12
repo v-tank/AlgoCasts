29addf914ea047bbbf801bfb756fbc7d67a8d363
Vaibhav Tank <vaibhav@MBP.local>
2018-08-11 17:29:34 -0700

    working mergeSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -41,11 +41,29 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  var results = [];
 
+  while (left.length > 0 && right.length > 0) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
