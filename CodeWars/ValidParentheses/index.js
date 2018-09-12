9c68005495d14a9bb2dbc3357092f343a8abc348
Vaibhav Tank <vaibhav@MBP.local>
2018-09-11 19:00:05 -0700

    update valid parens solution

--- CodeWars/ValidParentheses/index.js
+++ CodeWars/ValidParentheses/index.js
@@ -1,12 +1,35 @@
-function validParentheses(parens) {
-  var sum = 0;
+var isValid = function(s) {
+  var stack = [];
 
-  for (let paren of parens) {
-    if (paren === '(') sum += 1;
-    if (paren === ')') sum -= 1;
-    if (sum < 0) return false;
+  if (s.length % 2 !== 0) {
+    return false;
+  } else {
+    for (let paren of s) {
+      switch (paren) {
+        case '(':
+          stack.push(')');
+          break;
+        case '{':
+          stack.push('}');
+          break;
+        case '[':
+          stack.push(']');
+          break;
+        default:
+          var peek = stack[stack.length - 1];
+
+          if (peek !== paren) {
+            return false;
+          } else {
+            stack.pop();
+          }
+      }
+    }
+
+    if (stack.length > 0) {
+      return false;
+    }
+
+    return true;
   }
-
-  return sum === 0;
-}
-
+};
